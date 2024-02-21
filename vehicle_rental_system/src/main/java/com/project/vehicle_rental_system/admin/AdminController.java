4a47051b65a42cfbbd8d6ba651e4ca577f85348c
KavyaSreeNatti <kavyasreenatti2003@gmail.com>
2024-02-21 11:30:50 +0530

    admin changes updated

--- vehicle_rental_system/src/main/java/com/project/vehicle_rental_system/admin/AdminController.java
+++ vehicle_rental_system/src/main/java/com/project/vehicle_rental_system/admin/AdminController.java
@@ -1,12 +1,43 @@
 package com.project.vehicle_rental_system.admin;
 
+import com.project.vehicle_rental_system.customer.Customer;
+import com.project.vehicle_rental_system.customer.CustomerException;
+import com.project.vehicle_rental_system.customer.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("home")
 public class AdminController {
     @Autowired
     private AdminService adminService;
+
+    @PostMapping("customer/add")
+    public Customer addCustomer(@RequestBody Customer customer) throws CustomerException {
+        return adminService.addCustomer(customer);
+    }
+
+    @PutMapping("customer/update")
+    public Customer updateCustomer(@RequestBody Customer customer) throws CustomerException{
+        return adminService.updateCustomer(customer);
+    }
+
+    @GetMapping("{Id}")
+    public Customer getCustomer(@PathVariable("Id") Integer customerId) throws CustomerException{
+        return adminService.getCustomerbyId(customerId);
+    }
+
+    @GetMapping("AllCustomerDetails")
+    public List<Customer> getAllCustomers(){
+        return adminService.getAllCustomers();
+    }
+
+    @DeleteMapping("{id}")
+    public Customer deleteCustomer(@PathVariable("id")Integer customerId) throws CustomerException{
+        return adminService.deleteCustomer(customerId);
+    }
+
+
 }
--- vehicle_rental_system/src/main/java/com/project/vehicle_rental_system/admin/AdminServiceImplementation.java
+++ vehicle_rental_system/src/main/java/com/project/vehicle_rental_system/admin/AdminServiceImplementation.java
@@ -1,10 +1,97 @@
 package com.project.vehicle_rental_system.admin;
 
+import com.project.vehicle_rental_system.customer.Customer;
+import com.project.vehicle_rental_system.customer.CustomerException;
+import com.project.vehicle_rental_system.customer.CustomerRepository;
+import com.project.vehicle_rental_system.vehicle.Vehicle;
+import com.project.vehicle_rental_system.vehicle.VehicleException;
+import com.project.vehicle_rental_system.vehicle.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdminServiceImplementation implements AdminService{
     @Autowired
     private AdminRepository adminRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    @Override
+    public Admin loginAdmin(Admin admin) throws AdminException {
+        Optional<Admin> adminopt= adminRepository.findById(admin.getAdminId());
+        if(adminopt.isEmpty())
+        {
+            throw new AdminException("Admin not found....Please provide the valid details!!!");
+        }
+        Admin validatingAdmin= adminRepository.getById(admin.getAdminId());
+        try {
+            if (validatingAdmin.getAdminName().equals(admin.getAdminName())) {
+                try {
+                    if (validatingAdmin.getAdminPassword().equals(admin.getAdminPassword())) {
+                        return admin;
+                    }
+                    throw new RuntimeException();
+                }
+                catch(Exception e)
+                {
+                    throw new AdminException("Please provide the valid password....");
+                }
+
+            }
+            throw new RuntimeException();
+        }
+        catch(Exception e)
+        {
+            throw  new AdminException("Please provide the valid username.....");
+        }
+
+    }
+
+    @Override
+    public Customer addCustomer(Customer newCustomer) throws CustomerException {
+        Optional<Customer> customerOpt = this.customerRepository.findByCustomerEmail(newCustomer.getCustomerEmail());
+        if(customerOpt.isPresent())
+            throw new CustomerException("Email already registered, please re try."+newCustomer.getCustomerEmail());
+
+
+        return this.customerRepository.save(newCustomer);
+    }
+
+    @Override
+    public Customer updateCustomer(Customer newCustomer) throws CustomerException {
+        Optional<Customer> customerOpt = this.customerRepository.findByCustomerEmail(newCustomer.getCustomerEmail());
+        if(customerOpt.isEmpty())
+            throw new CustomerException("Email not found!"+newCustomer.getCustomerEmail());
+
+        return this.customerRepository.save(newCustomer);
+    }
+
+    @Override
+    public Customer getCustomerbyId(Integer id) throws CustomerException {
+        Optional<Customer> customerOpt = this.customerRepository.findById(id);
+        if(customerOpt.isEmpty())
+            throw new CustomerException("Customer id not found!!"+id);
+        return this.customerRepository.findById(id).get();
+    }
+
+    @Override
+    public Customer deleteCustomer(Integer id) throws CustomerException {
+        Optional<Customer> customerOpt = this.customerRepository.findById(id);
+        if(customerOpt.isEmpty())
+            throw new CustomerException("Customer id not found!!"+id);
+        this.customerRepository.deleteById(id);
+        return customerOpt.get();
+    }
+
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return this.customerRepository.findAll();
+    }
+
 }
