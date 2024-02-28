a7779c8941f2345edc157cd421808a6a7727f166
KavyaSreeNatti <kavyasreenatti2003@gmail.com>
2024-02-28 17:08:10 +0530

    admin changes

--- vehicle_rental_system/src/main/java/com/project/vehicle_rental_system/admin/AdminException.java
+++
@@ -1,7 +0,0 @@
-package com.project.vehicle_rental_system.admin;
-
-public class AdminException extends Exception{
-    public AdminException(String message) {
-        super(message);
-    }
-}
---
+++ vehicle_rental_system/src/main/java/com/project/vehicle_rental_system/admin/exceptions/UpdateCustomerException.java
@@ -0,0 +1,2 @@
+package com.project.vehicle_rental_system.admin.exceptions;public class UpdateCustomerException {
+}
